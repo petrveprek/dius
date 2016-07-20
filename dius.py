2ca4c690227868e7588824e9d40710517008cf72
Petr Vepřek <petr.veprek.cz420@gmail.com>
2016-07-20 17:17:45 +0100

    Work in progress

--- dius.py
+++ dius.py
@@ -2,32 +2,44 @@
 # Copyright (c) 2016 Petr Veprek
 """Disk Usage"""
 
-import math
-import operator
-import os
-import string
-import sys
-import time
+import enum, math, operator, os, string, sys, time
 
 TITLE = "Disk Usage"
 VERSION = "0.0.0"
 VERBOSE = False
-WIDTH = 80
 COUNT = 20
+WIDTH = 80
 
 def now(on="on", at="at"):
-    return "{}{} {}{}".format(on + " " if on != "" else "", time.strftime("%Y-%m-%d"), at + " " if at != "" else "", time.strftime("%H:%M:%S"))
+    return "{}{} {}{}".format(
+        on + " " if on != "" else "", time.strftime("%Y-%m-%d"),
+        at + " " if at != "" else "", time.strftime("%H:%M:%S"))
 
-def neat(str, max):
+def printable(str, max):
     str = "".join([char if char in string.printable else "_" for char in str])
     if len(str) > max: str = str[:max-3] + "..."
     return str
 
-def nice(num):
+class Mode(enum.Enum):
+    plain = 0
+    grouped = 1
+    gazillion = 2
+
+def grouped(num):
     return "{:,}".format(num)
 
-def digits(num, min=0, kmgt=False):
-    return len(nice(num)) if kmgt else max(min, math.ceil(math.log10(num)))
+def gazillion(num, suffix="B"):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if num < 1024.0:
+            return "{:5.1f}{}{}".format(num, unit, suffix)
+        num /= 1024.0
+    return "{:.1f}{}{}".format(num, 'Yi', suffix)
+
+def places(num, min=0, mode=Mode.plain):
+    return max(min,
+        len(grouped(num))   if mode==Mode.grouped else
+        len(gazillion(num)) if mode==Mode.gazillion else
+        math.ceil(math.log10(num)))
 
 def main():
     print("{} {}".format(TITLE, VERSION))
@@ -43,18 +55,18 @@ def main():
     print("Analyzing {}".format(top))
     usage = {}
     for path, dirs, files in os.walk(top):
-        print("\rScanning {: <{}}".format(neat(path, WIDTH), WIDTH), end="")
+        print("\rScanning {: <{}}".format(printable(path, WIDTH), WIDTH), end="")
         usage[path] = sum(map(os.path.getsize, filter(os.path.isfile, map(lambda file: os.path.join(path, file), files))))
     print("\r         {: <{}}\r".format("", WIDTH), end="")
     usage = sorted(usage.items(), key=operator.itemgetter(1), reverse=True)
-    digsCount = digits(len(usage), min=2)
+    widthCount = places(len(usage), min=2)
     total = sum(map(lambda pair: pair[1], usage))
-    digsTotal = digits(total, kmgt=True)
+    widthTotal = places(total, mode=Mode.gazillion)
     for i, (path, size) in enumerate(usage[:COUNT]):
-        print("{:{}}/{} {:>{}} {}".format(i+1, digsCount, len(usage), nice(size), digsTotal, path))
+        print("{:{}}/{} {:>{}} {}".format(i+1, widthCount, len(usage), gazillion(size), widthTotal, path))
     if (COUNT < len(usage)):
-        print("{:>{}} {:>{}}".format("OTHER", 2*digsCount+1, nice(sum(map(lambda pair: pair[1], usage[COUNT:]))), digsTotal))
-    print("{:>{}} {:>{}}".format("TOTAL", 2*digsCount+1, nice(total), digsTotal))
+        print("{:>{}} {:>{}}".format("OTHER", 2*widthCount+1, gazillion(sum(map(lambda pair: pair[1], usage[COUNT:]))), widthTotal))
+    print("{:>{}} {:>{}}".format("TOTAL", 2*widthCount+1, gazillion(total), widthTotal))
     
     if VERBOSE:
         elapsed = time.time() - start
